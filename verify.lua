3204976ccfe1a37f8cb32307ae8c3a8adb6b92aa
walksanatora <walkerffo22@gmail.com>
2023-01-10 11:21:46 -0800

    rename and update archive verifier program

---
+++ verify.lua
@@ -0,0 +1,45 @@
+local d = shell.getRunningProgram()
+local sc = require "cc.shell.completion"
+
+local pdir = fs.getDir(shell.getRunningProgram())
+package.path = package.path .. ";/" .. pdir .. "/?.lua;/" .. pdir .."/?;" .. pdir .. "?"
+
+local loaded_ld, ld = pcall(require, "LD")
+
+if not shell.getCompletionInfo()[d] then
+    shell.setCompletionFunction(d,sc.build(
+        sc.file
+    ))
+end
+
+if not loaded_ld then
+    error("failed to load stripped LibDeflate: "..ld)
+end
+
+local o = shell.resolve(...)
+if not fs.exists(o) then
+    print("verify.lua - VGZ and SEA archive verifier")
+    print("Usage:")
+    print("verify <archive>")
+    print("where <archive> is a VGZ/SEA archive")
+    print("this program will print what part of decompress fails")
+    return
+end
+
+local h = fs.open(o,'rb')
+local content = h.readAll()h.close()
+local data = content:match("%[===%[(.+)%]===%]") or content --extract from SEA or assume vgz
+print("content bytes:",#data)
+local decode = ld:d(data)
+if not decode then
+    print("unnable to decompress GZIP data, is it corrupt")
+    return
+end
+local t = textutils.unserialise(decode)
+if not t then
+    print("Archive is corrupted (could not decode table)")
+elseif type(t) ~= "table" then
+    print("Archive is corrupted (decompressed data is not a table)")
+else
+    print("Archive is valid")
+end
